d303746ff4f1c9b7341091d972ae13943133aabb
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-12-19 23:29:18 +0530

    Added underlined Hover effect to menu items

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,34 +1,34 @@
 import React from "react";
 import icon from "./logo.png";
 import menu from "./menu.png";
-function Navbar() {
+import "./Navbar.css"
 
-    return (
-        
-        <nav className="flex justify-between p-5 text-white absolute left-0 right-0 z-10 items-center font-bold text-[1vw] cursor-pointer">
-            <div className="aspect-w-4 aspect-h-1 w-[20vw]">
-                <img src={icon} alt="Spacex" />
-            </div>
-            
-                <ul className="hidden space-x-4 md:flex">
-                    <li>FALCON 9</li>
-                    <li>FALCON HEAVY</li>
-                    <li>DRAGON</li>
-                    <li>STARSHIP</li>
-                    <li>HUMAN SPACELIGHT</li>
-                    <li>RIDESHARE</li>
-                    <li>STARSHIELD</li>
-                    <li>STARSLINK</li>
-                </ul>
-            
-                <ul className="flex space-x-4">
-                    <li>SHOP</li>
-                    <li><img className="w-[1vw] pt-[0.4vw]" src={menu} alt="menu"/></li>
-                </ul>
-            
-        </nav>
-        
-    );
+function Navbar() {
+  return (
+    <nav className="flex justify-between p-5 text-white absolute left-0 right-0 z-10 items-center font-bold text-[1vw] cursor-pointer">
+      <div className="aspect-w-4 aspect-h-1 w-[20vw]">
+        <img src={icon} alt="Spacex" />
+      </div>
+
+      <ul className="hidden space-x-4 md:flex">
+        <li className="nav-item">FALCON 9</li>
+        <li className="nav-item">FALCON HEAVY</li>
+        <li className="nav-item">DRAGON</li>
+        <li className="nav-item">STARSHIP</li>
+        <li className="nav-item">HUMAN SPACELIGHT</li>
+        <li className="nav-item">RIDESHARE</li>
+        <li className="nav-item">STARSHIELD</li>
+        <li className="nav-item">STARSLINK</li>
+      </ul>
+
+      <ul className="flex space-x-4">
+        <li className="nav-item">SHOP</li>
+        <li>
+          <img className="w-[1vw] pt-[0.3vw]" src={menu} alt="menu" />
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
